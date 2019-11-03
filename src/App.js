6580b5dceddfee18ed9ec70a275ba2eab1ff2f47
dpaschal-lf <daniel.paschal@learningfuze.com>
2019-11-03 11:26:17 -0800

    got basic todo items loaded and listing example

--- src/App.js
+++ src/App.js
@@ -5,18 +5,37 @@ import './App.css';
 class App extends React.Component{
   constructor(props){
     super(props);
+    this.componentDidMount = this.componentDidMount.bind(this);
+    this.state = {
+      view: 'list',
+      data: []
+    }
   }
   componentDidMount(){
     fetch('http://localhost/lfz/_practice/todo_complete/public/api/server/todo_get.php')
       .then( response => response.json())
       .then( data => {
-        console.log(data);
+        this.setState({
+          data: data
+        })
       })
   }
+  view_list(){
+    return this.state.data.map( dataItem =>{
+              return(
+                <div key={dataItem.ID}>{dataItem.title}</div>
+              );
+            })
+
+  }
   render(){
+    const currentView = 'view_'+this.state.view;
     return(
-      <div>app</div>
-    );
+      <div>
+        <h3>Todo list</h3>
+        { this[currentView]()}
+      </div>
+    )
   }
 }
 
