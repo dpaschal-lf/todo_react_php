ba904693ef84af53b2bee91e8b6bb77263a0ad92
dpaschal-lf <daniel.paschal@learningfuze.com>
2019-11-03 11:17:55 -0800

    added basic request of data

--- public/api/server/todo_create.php
+++ public/api/server/todo_create.php
@@ -2,8 +2,17 @@
 
 require_once('mysqlconnect.php');
 
+$title = "title";
+$description = "description";
 
 
-$query = "INSERT INTO `todo_items` SET `name`=";
+$query = "INSERT INTO `todo_items` SET `title`='$title', `description`='$description', `status`='active', `created`=NOW(), `updated`=NOW()";
+
+$result = mysqli_query($conn, $query);
+
+if(mysqli_affected_rows($conn)>0){
+    $id = mysqli_insert_id($conn);
+    print(json_encode(['id'=>$id]));
+}
 
 ?>
